ce9a6b9e1222ce1d607fcad6e912fc3a308aefed
ChinjiroO <techinseoikrathok@gmail.com>
2021-12-26 18:10:16 +0700

    #fix function move items[] => itemCompleted[]

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ IconBtn.style.transform = "rotate(360deg)";
 
 // * Event listeners
 document.addEventListener("DOMContentLoaded", getLocalStorageItem);
+document.addEventListener("DOMContentLoaded", getCompletedList);
 submitBtn.addEventListener("click", addTodo);
 dropdownToggle.addEventListener("click", showDropdown);
 document.addEventListener("click", insideDropdownMenu);
@@ -19,6 +20,7 @@ BtnComplete.addEventListener("click", handleClickComplete);
 List.addEventListener("click", deleteItem);
 List.addEventListener("click", updateItem);
 List.addEventListener("click", checkComplete);
+// completeList.addEventListener("click", checkTarget);
 
 // * ✅ DONE: Create Todo
 function addTodo(e) {
@@ -82,7 +84,7 @@ function saveToLocalStorage(item) {
   localStorage.setItem("items", JSON.stringify(items));
 }
 
-// * : Show items[] from LocalStorge
+// * ✅ DONE: Show items[] from LocalStorge
 function getLocalStorageItem(item) {
   let items;
   if (localStorage.getItem("items") === null) {
@@ -131,30 +133,75 @@ function getLocalStorageItem(item) {
     List.appendChild(Div);
   });
 }
-
-// TODO: ✏️  move items[] ==> itemsComplete
-function checkComplete(e) {
-  const item = e.target;
-  if (item.classList[0] === "checkbox") {
-    let todo_item = item.parentElement.parentElement;
-    completeLocal(todo_item);
-    deleteItemFromLocal(todo_item);
-    todo_item.remove();
-    console.log(todo_item.children[0].innerText);
-  }
-}
-function completeLocal(todo) {
+// TODO: ✏️ ReCompleted list
+// function checkTarget(e) {
+//   const item = e.target;
+//   if (item.classList == "checkbox complete") {
+//     let selected = item.parentElement.children[1].innerText;
+//     console.log(selected);
+//     reComplete(selected);
+//     item.parentElement.parentElement.remove();
+//   }
+// }
+// function reComplete(item) {
+//   let items;
+//   if (localStorage.getItem("itemCompleted") === null) {
+//     items = [];
+//   } else {
+//     items = JSON.parse(localStorage.getItem("itemCompleted"));
+//   }
+//   items.splice(items.indexOf(item), 1);
+//   localStorage.setItem("itemCompleted", JSON.stringify(items));
+// }
+// TODO: ✏️  Get completed list
+function getCompletedList() {
   let itemCompleted;
   if (localStorage.getItem("itemCompleted") === null) {
     itemCompleted = [];
   } else {
     itemCompleted = JSON.parse(localStorage.getItem("itemCompleted"));
   }
-  itemCompleted.push(todo.children[0].innerText);
-  localStorage.setItem("itemCompleted", JSON.stringify(itemCompleted));
+  itemCompleted.forEach(function (item) {
+    const Div = document.createElement("div");
+    Div.classList.add("todo-item");
+    const list = document.createElement("li");
+    list.classList.add("item");
+    const checkbox = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    checkbox.setAttribute("viewBox", "0 0 69 69");
+    checkbox.classList.add("checkbox", "complete");
+    const circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    circle.setAttribute("cx", 34.5);
+    circle.setAttribute("cy", 34.5);
+    circle.setAttribute("r", 34.5);
+    circle.setAttribute("stroke", "black");
+    circle.setAttribute("stroke-width", "5");
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute(
+      "d",
+      "M26.128 42.0506C24.929 43.4698 22.7412 43.469 21.5433 42.0487L8.33965 26.3952C7.1438 24.9775 4.96086 24.9737 3.76007 26.3873L1.64407 28.8782C0.695181 29.9952 0.692324 31.6344 1.63732 32.7547L21.5419 56.3525C22.7403 57.7733 24.9291 57.7735 26.1278 56.353L67.2286 7.6462C68.2312 6.45811 68.1601 4.70099 67.0648 3.5978L64.8072 1.32387C63.567 0.0746672 61.5227 0.156777 60.3866 1.50143L26.128 42.0506Z"
+    );
+    path.setAttribute("fill", "#3180FF");
+    const title = document.createElement("label");
+    title.innerText = item;
+    title.classList.add("title");
+    const trashIcon = document.createElement("i");
+    trashIcon.classList.add("fas", "fa-trash-alt", "del-icon");
+
+    checkbox.appendChild(circle);
+    checkbox.appendChild(path);
+    list.appendChild(checkbox);
+    list.appendChild(title);
+    list.appendChild(trashIcon);
+    Div.appendChild(list);
+    completeList.appendChild(Div);
+  });
 }
-// TODO: ✏️  Get completed list
-function getCompletedList() {}
 // TODO: ✏️  Update Todo
 function updateItem(e) {
   let item = e.target;
@@ -186,24 +233,83 @@ function updateItemFromLocal(item) {
     items = [];
   } else items = JSON.parse(localStorage.getItem("items"));
 }
+// * ✅ DONE: move items[] ==> itemCompleted[]
+function checkComplete(e) {
+  const item = e.target;
+  if (item.classList[0] === "checkbox") {
+    let todo_item = item.parentElement.parentElement;
+    completeLocal(todo_item);
+    deleteItemFromLocal(todo_item);
+    todo_item.remove();
+    const Div = document.createElement("div");
+    Div.classList.add("todo-item");
+    const list = document.createElement("li");
+    list.classList.add("item");
+    const checkbox = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    checkbox.setAttribute("viewBox", "0 0 69 69");
+    checkbox.classList.add("checkbox", "complete");
+    const circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    circle.setAttribute("cx", 34.5);
+    circle.setAttribute("cy", 34.5);
+    circle.setAttribute("r", 34.5);
+    circle.setAttribute("stroke", "black");
+    circle.setAttribute("stroke-width", "5");
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute(
+      "d",
+      "M26.128 42.0506C24.929 43.4698 22.7412 43.469 21.5433 42.0487L8.33965 26.3952C7.1438 24.9775 4.96086 24.9737 3.76007 26.3873L1.64407 28.8782C0.695181 29.9952 0.692324 31.6344 1.63732 32.7547L21.5419 56.3525C22.7403 57.7733 24.9291 57.7735 26.1278 56.353L67.2286 7.6462C68.2312 6.45811 68.1601 4.70099 67.0648 3.5978L64.8072 1.32387C63.567 0.0746672 61.5227 0.156777 60.3866 1.50143L26.128 42.0506Z"
+    );
+    path.setAttribute("fill", "#3180FF");
+    const title = document.createElement("label");
+    title.innerText = todo_item.children[0].innerText;
+    title.classList.add("title");
+    const trashIcon = document.createElement("i");
+    trashIcon.classList.add("fas", "fa-trash-alt", "del-icon");
+
+    checkbox.appendChild(circle);
+    checkbox.appendChild(path);
+    list.appendChild(checkbox);
+    list.appendChild(title);
+    list.appendChild(trashIcon);
+    Div.appendChild(list);
+    completeList.appendChild(Div);
+  }
+}
+function completeLocal(todo) {
+  let itemCompleted;
+  if (localStorage.getItem("itemCompleted") === null) {
+    itemCompleted = [];
+  } else {
+    itemCompleted = JSON.parse(localStorage.getItem("itemCompleted"));
+  }
+  itemCompleted.push(todo.children[0].innerText);
+  localStorage.setItem("itemCompleted", JSON.stringify(itemCompleted));
+}
 // * ✅ DONE: Delete Todo
 function deleteItem(e) {
   const item = e.target;
   if (item.classList == "fas fa-trash-alt del-icon") {
     const todo = item.parentElement.parentElement;
     deleteItemFromLocal(todo);
-    todo.remove();
   }
 }
 function deleteItemFromLocal(item) {
   let items;
   if (localStorage.getItem("items") === null) {
     items = [];
-  } else items = JSON.parse(localStorage.getItem("items"));
-
+  } else {
+    items = JSON.parse(localStorage.getItem("items"));
+  }
   const index = item.children[0].innerText;
   items.splice(items.indexOf(index), 1);
   localStorage.setItem("items", JSON.stringify(items));
+  item.remove();
 }
 
 // TODO: ✏️ Show complete list on click
